38d0a555b33a746743965718f0c5cdc28c011d86
TT <trey@88oak.com>
2020-09-12 16:25:57 -0500

    Initial Commit

---
+++ Shared/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Shared
+//
+//  Created by Trey Tartt on 9/12/20.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
