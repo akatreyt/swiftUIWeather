291aabe195c3dc83dd3bf7431d7b642f20d900d5
TT <trey@88oak.com>
2020-09-16 15:27:10 -0500

    widget formatting

--- WeatherWidget/WeatherWidget.swift
+++ WeatherWidget/WeatherWidget.swift
@@ -53,21 +53,15 @@ struct WeatherWidgetEntryView : View {
         let periodsToShow = getPeriods(forSizeClass: family,
                                        allPeriods: forecast.periods)
         
-        if periodsToShow.count > 0{
-            HStack{
-                ForEach(periodsToShow, id: \.id){ period in
-                    VStack{
-                        Text(rowFormatter.degreeToString(fromPeriod: period, forTemp: .Fahrenheit))
-                            .font(.body)
-                            .foregroundColor(Color("TextColor"))
-                        Image(systemName: "cloud")
-                            .foregroundColor(Color("TextColor"))
-                        Text(rowFormatter.degreeToString(fromPeriod: period, forTemp: .Celcius))
-                            .font(.body)
-                            .foregroundColor(Color("TextColor"))
-                    }
-                }
-            }
+        switch family {
+        case .systemSmall:
+            SmallView(periods: periodsToShow)
+        case .systemMedium:
+            MediumView(periods: periodsToShow)
+        case .systemLarge:
+            LargeView(periods: periodsToShow)
+        @unknown default:
+            SmallView(periods: periodsToShow)
         }
     }
     
@@ -76,15 +70,145 @@ struct WeatherWidgetEntryView : View {
         
         switch sizeClass {
         case .systemSmall:
-            returnPeriods = Array(periods.prefix(upTo: 2))
+            returnPeriods = Array(periods.prefix(upTo: 1))
         case .systemMedium:
-            returnPeriods = Array(periods.prefix(upTo: 5))
+            returnPeriods = Array(periods.prefix(upTo: 3))
         case .systemLarge:
-            returnPeriods = Array(periods.prefix(upTo: 8))
+            returnPeriods = Array(periods.prefix(upTo: 6))
         @unknown default:
             returnPeriods = Array(periods.prefix(upTo: 2))
         }
         return returnPeriods
+    }
+}
+
+struct SmallView : View {
+    let periods : [Forecast.Period]
+    let rowFormatter : RowFormattable.Type = RowFormatter.self
+    
+    var body: some View {
+        HStack{
+            ForEach(periods, id: \.id){ period in
+                VStack{
+                    Text(period.name ?? "")
+                        .font(.body)
+                        .foregroundColor(Color("TextColor"))
+                        .multilineTextAlignment(.center)
+                    Text(rowFormatter.degreeToString(fromPeriod: period, forTemp: .Fahrenheit))
+                        .font(.body)
+                        .foregroundColor(Color("TextColor"))
+                    Image(systemName: "cloud")
+                        .foregroundColor(Color("TextColor"))
+                    Text(rowFormatter.degreeToString(fromPeriod: period, forTemp: .Celcius))
+                        .font(.body)
+                        .foregroundColor(Color("TextColor"))
+                }
+                Spacer()
+            }
+        }
+        .padding()
+    }
+}
+
+struct MediumView : View {
+    let periods : [Forecast.Period]
+    let rowFormatter : RowFormattable.Type = RowFormatter.self
+    var dateIntervalFormatter = DateIntervalFormatter(){
+        didSet{
+            dateIntervalFormatter.dateStyle = .short
+            dateIntervalFormatter.timeStyle = .medium
+        }
+    }
+    
+    var body: some View {
+        HStack{
+            ForEach(periods, id: \.id){ period in
+                VStack{
+                    Text(period.name ?? "")
+                        .font(.body)
+                        .foregroundColor(Color("TextColor"))
+                        .multilineTextAlignment(.center)
+                    Text(rowFormatter.degreeToString(fromPeriod: period, forTemp: .Fahrenheit))
+                        .font(.body)
+                        .foregroundColor(Color("TextColor"))
+                    Image(systemName: "cloud")
+                        .foregroundColor(Color("TextColor"))
+                    Text(rowFormatter.degreeToString(fromPeriod: period, forTemp: .Celcius))
+                        .font(.body)
+                        .foregroundColor(Color("TextColor"))
+                    Text(period.shortForecast)
+                        .font(.body)
+                        .foregroundColor(Color("TextColor"))
+                }
+                Spacer()
+            }
+        }
+        .padding()
+    }
+}
+
+struct LargeView : View {
+    let periods : [Forecast.Period]
+    let rowFormatter : RowFormattable.Type = RowFormatter.self
+    
+    var body: some View {
+        if periods.count != 6{
+            Text("Incorrect info")
+        }else{
+            let firstTrip = Array(periods.prefix(upTo: 3))
+            let lastTrip = Array(periods.suffix(3))
+            
+            VStack{
+                HStack{
+                    Spacer()
+                    ForEach(firstTrip, id: \.id){ period in
+                        VStack{
+                            Text(period.name ?? "")
+                                .font(.body)
+                                .foregroundColor(Color("TextColor"))
+                                .multilineTextAlignment(.center)
+                            Text(rowFormatter.degreeToString(fromPeriod: period, forTemp: .Fahrenheit))
+                                .font(.body)
+                                .foregroundColor(Color("TextColor"))
+                            Image(systemName: "cloud")
+                                .foregroundColor(Color("TextColor"))
+                            Text(rowFormatter.degreeToString(fromPeriod: period, forTemp: .Celcius))
+                                .font(.body)
+                                .foregroundColor(Color("TextColor"))
+                            Text(period.shortForecast)
+                                .font(.body)
+                                .foregroundColor(Color("TextColor"))
+                        }
+                        Spacer()
+                    }
+                }
+                Spacer()
+                HStack{
+                    Spacer()
+                    ForEach(lastTrip, id: \.id){ period in
+                        VStack{
+                            Text(period.name ?? "")
+                                .font(.body)
+                                .foregroundColor(Color("TextColor"))
+                                .multilineTextAlignment(.center)
+                            Text(rowFormatter.degreeToString(fromPeriod: period, forTemp: .Fahrenheit))
+                                .font(.body)
+                                .foregroundColor(Color("TextColor"))
+                            Image(systemName: "cloud")
+                                .foregroundColor(Color("TextColor"))
+                            Text(rowFormatter.degreeToString(fromPeriod: period, forTemp: .Celcius))
+                                .font(.body)
+                                .foregroundColor(Color("TextColor"))
+                            Text(period.shortForecast)
+                                .font(.body)
+                                .foregroundColor(Color("TextColor"))
+                        }
+                        Spacer()
+                    }
+                }
+            }
+            .padding()
+        }
     }
 }
 
