afc9533d2aea9905fce6284281071a2662de0398
TT <trey@88oak.com>
2020-09-17 15:21:21 -0500

    add app group to apps and widget for sharing data

--- Shared/Data/Storage.swift
+++ Shared/Data/Storage.swift
@@ -9,6 +9,17 @@ import Foundation
 import CoreLocation
 import NationalWeatherService
 
+public enum AppGroup: String {
+  case weather = "group.com.88oak.weather"
+
+  public var containerURL: URL {
+    switch self {
+    case .weather:
+      return FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: self.rawValue)!
+    }
+  }
+}
+
 protocol Storable {
     init()
     static func save(completeWeather cw : CompleteWeather) throws
@@ -31,14 +42,14 @@ public class CoreDataStorage : Storable {
 public class PlistStorage : Storable{
     required init() {}
     
-    static let fileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Weather.plist")
+    static let fileURL = AppGroup.weather.containerURL.appendingPathComponent("weather.plist")
     
     static var encoder = PropertyListEncoder()
     static var decoder = PropertyListDecoder()
     
     static func getLatestForcast() throws -> CompleteWeather {
         do {
-            let data = try Data.init(contentsOf: fileURL!)
+            let data = try Data.init(contentsOf: PlistStorage.fileURL)
             return try decoder.decode(CompleteWeather.self, from: data)
         } catch {
             throw error
@@ -48,7 +59,7 @@ public class PlistStorage : Storable{
     static func save(completeWeather cw : CompleteWeather) throws{
         do {
             let data = try encoder.encode(cw)
-            try data.write(to: PlistStorage.fileURL!)
+            try data.write(to: PlistStorage.fileURL)
         } catch {
             throw error
         }
